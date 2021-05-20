c692c7a07f9b011473ae89f98653ef8ad2f782ad
Fabio <fabio.rdrgs@eduge>
2021-05-20 14:53:40 +0200

    Update signup.js

--- Projet/Search-Find-Job/js/signup.js
+++ Projet/Search-Find-Job/js/signup.js
@@ -1,11 +1,15 @@
+//Variables
 var emailInput = $("#email");
 var passwordInput = $("#pswd");
 var passwordVerifyInput = $("#pswd2");
 
+//Effectue une action lorsqu'une touche est pressée dans un champ input
 $("input").on('keyup',function(){
+    //Récupère les valeurs des champs email, mot de passe et vérification de mot de passe
     var email = emailInput.val();
     var pswd = passwordInput.val();
     var pswdVer = passwordVerifyInput .val();
+    //Teste si le mail est conforme aux normes de mail usuelles. Si oui, enlève la couleur du fond, si non, mets le fond en rouge
     if(email.length > 0 && email.search(/^([\w\d._\-#])+@([\w\d._\-#]+[.][\w\d._\-#]+)+$/) != -1)
     emailInput.css('background-color',"");
     else
@@ -13,7 +17,7 @@ $("input").on('keyup',function(){
         emailInput.css('background-color',"red");  
         return;
     }
-
+    //Teste si un mot de passe est rentré, si oui, enlève la couleur du fond, si non, met le fond en rouge
     if(pswd.length > 0)   
     passwordInput.css('background-color',"");
     else
@@ -21,7 +25,7 @@ $("input").on('keyup',function(){
         passwordInput.css('background-color',"red");  
         return;
     }
-
+    //Teste si les mots de passes sont identiques. Si oui, envlève la couleur du fond, si non, mets le fond en rouge
     if(pswd == pswdVer)
     {
         passwordVerifyInput.css('background-color',"")     
@@ -31,7 +35,7 @@ $("input").on('keyup',function(){
         passwordVerifyInput.css('background-color',"red");
         return;
     }
-
+    //Si les mots de passes sont identiques et non vides et que le mail respecte les normes de mail, active le bouton s'enregistrer
     if(pswd.length > 0&&pswd == pswdVer&&email.length > 0 && email.search(/^([\w\d._\-#])+@([\w\d._\-#]+[.][\w\d._\-#]+)+$/) != -1)
     {
         $("#register").prop('disabled',false);
